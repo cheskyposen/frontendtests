b5bfd9af924d202103562973bb53ad9664888c86
eli braun <elim.braun@gmail.com>
2019-07-16 22:12:03 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,MatInputModule} from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
+import { ChildFormComponent } from './child-form/child-form.component';
+import {ReactiveFormsModule} from '@angular/forms'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    ChildFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
